620f9fdd248ac7107597009e40061143e6f32afd
Nitin P Kumar <nitin.p@imaginea.com>
2012-09-13 14:27:44 +0530

    Query template

--- HiveQctree/src/main/java/com/imaginea/qctree/Launch.java
+++ HiveQctree/src/main/java/com/imaginea/qctree/Launch.java
@@ -1,5 +1,7 @@
 package com.imaginea.qctree;
 
+import com.imaginea.qctree.hadoop.QCTree;
+import com.imaginea.qctree.hadoop.Query;
 import com.imaginea.qctree.hive.Hivejdbc;
 
 public class Launch {
@@ -12,7 +14,15 @@ public class Launch {
 		
 		Hivejdbc obj = Hivejdbc.getObject();
 		//obj.buildQCube();
-		System.out.println(obj.getTree());
+		//System.out.println(obj.getTree());
 		
+		Query query = new Query();
+		QCTree tree = obj.getTree();
+		
+		query.execute(tree, "*,Microsoft,*");
+		System.out.println("-------------");
+		query.execute(tree, "P2,Altavista,*");
+		System.out.println("-------------");
+		query.execute(tree, "*,*,p");
 	}
 }
